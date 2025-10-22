35f8c80023835607542c337a77ce991ffd1fa3b4
MedMejri1 <mejrief@gmail.com>
2025-10-22 15:21:28 +0100

    done

--- assets/js/ajax-form.js
+++ assets/js/ajax-form.js
@@ -40,9 +40,7 @@
 			// Set the message text.
 			if (data.responseText !== '') {
 				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
+			} 
 		});
 	});
 
